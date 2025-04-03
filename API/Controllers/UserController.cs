565daf28ee435107913ee72bc5993f08af851773
Venkata Samala <venkata_samala@outlook.com>
2025-04-03 16:34:18 -0700

    API Changes

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UserController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _dataContext.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+            var user = await _dataContext.Users.Where(s => s.Id == id ).FirstOrDefaultAsync();
+
+            if (user == null) {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace API.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
